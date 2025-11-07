d2b227c80eb0b8df4895e58eefa050ba39f851a8
Julz Aves <julzchristianb.aves@gmail.com>
2025-11-07 21:18:28 +0800

    AHHHHHHHH

--- flight_predictor/settings.py
+++ flight_predictor/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECRET_KEY = 'your-secret-key-here'  # Change in production
+SECRET_KEY = 'your-secret-key-here'  # Change in production
 
 DEBUG = False
 
